00ab07b36e52462b0dddb882aec1e85cca24eb3c
HGNogueira <henrique.nogueira@tecnico.ulisboa.pt>
2017-05-06 19:09:19 +0100

    Bug fixes, working version

--- client.c
+++ client.c
@@ -4,18 +4,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+
 #include "messages.h"
+#include "clientAPI.h"
 
 #define GATE_PORT 3000
 #define GATE_ADDR "127.0.0.1"
 
 int main(){
-    int s;
+    int s, err;
 	char scanned[MESSAGE_LEN];
 	FILE *f;
 	char fread_buff[50];
     int  port;
     in_port_t port_s;
+    message_gw gw_msg;
 	message rmsg;
 	message smsg;
 		
@@ -35,15 +40,17 @@ int main(){
 			pthread_exit(NULL);
 		}
 		else if(err == 0){ //server disconnected
-			printf("Server stopped responding\n");
+            //let gateway know that server stopped responding
+            /*
 			gw_msg.type = 1; //comunicate server stopped responding to gateway
-            
-			gw_msg.ID = ID;
 			if( (sendto(s_gw, &gw_msg, sizeof(gw_msg), 0,(const struct sockaddr *) &gw_addr, sizeof(gw_addr)) )==-1){
 				perror("GW contact");
 			}
-			close(scl);
-			return(NULL);
+            */
+
+			printf("Server stopped responding\n");
+			close(s);
+            exit(EXIT_FAILURE);
 		}
 		printf("sent %s --- received %s\n", smsg.buffer, rmsg.buffer);
 	}
