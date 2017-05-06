1603917ffb297691b2a389e49556e18b4e17c0a9
HGNogueira <henrique.nogueira@tecnico.ulisboa.pt>
2017-05-06 18:40:46 +0100

    2 sockets, 1 for client-side, 1 for peer-side interactions

--- gateway.c
+++ gateway.c
@@ -10,78 +10,37 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <ctype.h>
+#include <pthread.h>
+
 #include "messages.h"
 #include "serverlist.h"
 
+#define CLIENT_SIDE_PORT 3000
+#define PEER_SIDE_PORT 3001
+
 int run = 1;
+int sc, sp; // client side and server side socked descriptors
+serverlist *servers;  // linked list of servers, auxiliary node
+int ID = 0; /*server ID counter */
+
 void sigint_handler(int n){
-	run = 0;
+    close(sc);
+    close(sp);
+    printf("Gateway terminating\n");
+    exit(EXIT_SUCCESS);
 }
 
-int main(){
-	int s, err;
-	FILE *f;
-	struct sockaddr_in gw_addr;
-	struct sockaddr_in rmt_addr;
-	socklen_t addr_len;
-	message_gw gw_msg;
-	char fread_buff[50], client_send_buffer[50];
-	int port, available;
-	struct sigaction act_INT, act_SOCK;
-	serverlist *servers, *tmp_node;
-	int ID = 0; /*server ID counter */
+/* thread that interacts with client requests, receives socket descriptor */
+void *c_interact(void *dummy){
+    socklen_t addr_len;
+    struct sockaddr_in rmt_addr;
+    serverlist *tmp_node;
+    message_gw gw_msg;
 
-/****** SIGNAL MANAGEMENT ******/
-	act_INT.sa_handler = sigint_handler;
-	sigemptyset(&act_INT.sa_mask);
-	act_INT.sa_flags=0;
-	sigaction(SIGINT, &act_INT, NULL);
-
-	act_SOCK.sa_handler = sigint_handler;
-	sigemptyset(&act_SOCK.sa_mask);
-	act_SOCK.sa_flags=0;
-	sigaction(SIGPIPE, &act_SOCK, NULL); //Quando cliente fecha scl, podemos controlar comportamento do servidor
-/****** SIGNAL MANAGEMENT ******/
-
-/****** INITIALIZE SOCKET ******/
-	gw_addr.sin_family = AF_INET;
-
-	if( (f = fopen("./gw-server.txt", "r"))== NULL){
-		perror("fopen");
-		exit(EXIT_FAILURE);
-	}
-	fgets(fread_buff, 50, f);//lê porto
-	sscanf(fread_buff, "%d", &port);
-	printf("Gateway port: %d\n", port);
-	gw_addr.sin_port = htons(port);
-	gw_addr.sin_addr.s_addr = INADDR_ANY;
-	fclose(f);
-	
-	if( (s=socket(AF_INET, SOCK_DGRAM, 0)) == -1){
-		perror("socket error");
-		exit(EXIT_FAILURE);
-	}
-	
-	if( (bind(s, (const struct sockaddr *) &gw_addr, sizeof(gw_addr))) == -1){
-		perror("bind error");
-		exit(EXIT_FAILURE);
-	}
-
-/****** DGRAM SOCKET INITIALIZED ******/
-	
-
-/****** READY TO TALK TO CLIENT AND SERVER ******/
-	servers = init_server();
-	while(run){
+    while(1){
 		addr_len = sizeof(rmt_addr);
-		recvfrom(s, &gw_msg, sizeof(gw_msg), 0, (struct sockaddr *) &rmt_addr, &addr_len);
-		if(gw_msg.type == 1){ //contacted by peer
-			add_server(&servers, inet_ntoa(rmt_addr.sin_addr), gw_msg.port, ID);
-			sendto(s, &ID, sizeof(ID), 0, (const struct sockaddr*) &rmt_addr, sizeof(rmt_addr)); //send back ID information
-			ID++;
-			printf("New server available - addr=%s, port =%d\n", servers->address, servers->port);
-		}
-		else if(gw_msg.type == 0){ //contacted by client
+		recvfrom(sc, &gw_msg, sizeof(gw_msg), 0, (struct sockaddr *) &rmt_addr, &addr_len);
+		if(gw_msg.type == 0){ //contacted by client
 			printf("Contacted by new client\n");
 			if( (tmp_node = pick_server(servers)) != NULL){
 				printf("Server available with port %d\n", tmp_node->port);
@@ -89,28 +48,106 @@ int main(){
 				strcpy(gw_msg.address, tmp_node->address);
 				gw_msg.port = tmp_node->port;
 				tmp_node->nclients = tmp_node->nclients + 1;
-				sendto(s, &gw_msg, sizeof(gw_msg), 0, (const struct sockaddr*) &rmt_addr, sizeof(rmt_addr));
+				sendto(sc, &gw_msg, sizeof(gw_msg), 0, (const struct sockaddr*) &rmt_addr, sizeof(rmt_addr));
 			}
 			else{
 				printf("No servers available\n");
 				gw_msg.type = 0; // notify no server available
-				sendto(s, NULL, 0, 0, (const struct sockaddr*) &rmt_addr, sizeof(rmt_addr));
+				sendto(sc, NULL, 0, 0, (const struct sockaddr*) &rmt_addr, sizeof(rmt_addr));
 			}
 
+		} 
+        else{
+            printf("Received message from client with non-defined type %d\n", gw_msg.type);
+        }
+	}
+}
+
+/* thread that interacts with peer requests, receives socket descriptor */
+void *p_interact(void *dummy){
+    socklen_t addr_len;
+    struct sockaddr_in rmt_addr;
+    serverlist *tmp_node;
+    message_gw gw_msg;
+
+    while(1){
+		addr_len = sizeof(rmt_addr);
+		recvfrom(sp, &gw_msg, sizeof(gw_msg), 0, (struct sockaddr *) &rmt_addr, &addr_len);
+		if(gw_msg.type == 1){ //contacted by peer
+			add_server(&servers, inet_ntoa(rmt_addr.sin_addr), gw_msg.port, ID);
+			sendto(sp, &ID, sizeof(ID), 0, (const struct sockaddr*) &rmt_addr, sizeof(rmt_addr)); //send back ID information
+			ID++;
+			printf("New server available - addr=%s, port =%d\n", servers->address, servers->port);
 		}
-			
-		else if(gw_msg.type == 3){   //server lost 1 connection
+		else if(gw_msg.type == 2){   //server lost 1 connection
 			if(!(tmp_node = search_server(servers, gw_msg.ID)))
 				printf("Can't find server in list\n");
 			else
 				tmp_node->nclients = tmp_node->nclients - 1;
 			printf("Updated information from server\n");
 		}
+        else{
+            printf("Received message from peer with non-defined type %d\n", gw_msg.type);
+        }
 	}
-/****** END OF EXECUTION ********/
-	
-	printf("Closing socket\n");
-	close(s);
 
-	exit(EXIT_SUCCESS);
+}
+
+int main(){
+	int err;
+	struct sockaddr_in gwc_addr, gwp_addr; //client and peer side addr
+	socklen_t addr_len;
+	struct sigaction act_INT;
+    pthread_t client_side, peer_side;
+
+/****** SIGNAL MANAGEMENT ******/
+	act_INT.sa_handler = sigint_handler;
+	sigemptyset(&act_INT.sa_mask);
+	act_INT.sa_flags=0;
+	sigaction(SIGINT, &act_INT, NULL);
+/****** SIGNAL MANAGEMENT ******/
+        
+	servers = init_server();  // initialize server list
+
+/****** INITIALIZE client side socket ******/
+	gwc_addr.sin_family = AF_INET;
+	gwc_addr.sin_port = htons(CLIENT_SIDE_PORT);
+	gwc_addr.sin_addr.s_addr = INADDR_ANY;
+	
+	if( (sc=socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+		perror("socket error");
+		exit(EXIT_FAILURE);
+	}
+	
+	if( (bind(sc, (const struct sockaddr *) &gwc_addr, sizeof(gwc_addr))) == -1){
+		perror("bind error");
+		exit(EXIT_FAILURE);
+	}
+    if( pthread_create(&peer_side, NULL, c_interact, NULL ) != 0){
+				printf("Error creating a new thread\n");
+                exit(EXIT_FAILURE);
+    }
+
+/****** INITIALIZE peer side socket ******/
+	gwp_addr.sin_family = AF_INET;
+	gwp_addr.sin_port = htons(PEER_SIDE_PORT);
+	gwp_addr.sin_addr.s_addr = INADDR_ANY;
+	
+	if( (sp=socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+		perror("socket error");
+		exit(EXIT_FAILURE);
+	}
+	
+	if( (bind(sp, (const struct sockaddr *) &gwp_addr, sizeof(gwp_addr))) == -1){
+		perror("bind error");
+		exit(EXIT_FAILURE);
+	}
+
+    if( pthread_create(&peer_side, NULL, p_interact,NULL ) != 0){
+				printf("Error creating a new thread\n");
+                exit(EXIT_FAILURE);
+    }
+    
+/****** END OF EXECUTION ********/
+    pause(); //pause indefinately until SIGINIT
 }
