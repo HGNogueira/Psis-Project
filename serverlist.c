54209ebb50d5356f7a3567adabcf5b68b369c3e1
HGNogueira <henrique.nogueira@tecnico.ulisboa.pt>
2017-05-07 14:03:06 +0100

    Functions implemented with Mutex

--- serverlist.c
+++ serverlist.c
@@ -1,6 +1,8 @@
 #include "serverlist.h"
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
 
 /* inicialização da lista de servidores */
 serverlist *init_server(){
@@ -8,7 +10,7 @@ serverlist *init_server(){
 }
 
 /* função que escolhe servidor com menos carga */
-serverlist *pick_server(struct node *servers){
+serverlist *pick_server(struct node *servers, pthread_mutex_t *list_key){
 	struct node *s1,*s2;
 	int clients;
 
@@ -17,17 +19,20 @@ serverlist *pick_server(struct node *servers){
 
 	s1 = servers;
 	s2 = servers;
+    
+    pthread_mutex_lock(list_key); //danger of server stop existing
 	while(s1->next){
 		if(s2->nclients > s1->next->nclients){
 			s2 = s1->next;
 		}
 		s1 = s1->next;
 	}
+    pthread_mutex_unlock(list_key);
 
 	return s2;
 }
 
-void add_server(serverlist **servers, char *address, int port, int ID){
+void add_server(serverlist **servers, char *address, int port, int ID, pthread_mutex_t *list_key){
 	serverlist *newnode;
 
 	newnode = (serverlist *) malloc(sizeof(serverlist));
@@ -36,12 +41,16 @@ void add_server(serverlist **servers, char *address, int port, int ID){
 	newnode->ID = ID;
 	newnode->nclients = 0;
 
+    pthread_mutex_lock(list_key);
 	newnode->next = *servers;
 	*servers = newnode;
+    pthread_mutex_unlock(list_key);
 }
 
-serverlist *search_server(serverlist *servers, int ID){
+serverlist *search_server(serverlist *servers, int ID, pthread_mutex_t *list_key){
 	struct node *s;
+    
+    pthread_mutex_lock(list_key);
 	s = servers;
 	if(s == NULL)
 		return NULL;
@@ -50,5 +59,6 @@ serverlist *search_server(serverlist *servers, int ID){
 			return s;
 		s = s->next;
 	}
+    pthread_mutex_unlock(list_key);
 	return NULL;
 }
--- serverlist.h
+++ serverlist.h
@@ -1,3 +1,6 @@
+#include <pthread.h>
+#include <sys/types.h>
+
 typedef struct node{
 	int port;
 	char address[50];
@@ -9,7 +12,7 @@ typedef struct node{
 
 
 serverlist *init_server();
-serverlist *pick_server(serverlist *);//choose server according to least load rule
-void add_server(serverlist **servers, char *address, int port, int ID);
-serverlist *search_server(serverlist *servers, int ID);
+serverlist *pick_server(serverlist *, pthread_mutex_t *list_key);//choose server according to least load rule
+void add_server(serverlist **servers, char *address, int port, int ID, pthread_mutex_t *list_key);
+serverlist *search_server(serverlist *servers, int ID, pthread_mutex_t *list_key);
 
