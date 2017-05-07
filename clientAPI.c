e64bbfb79305200c759858be0e5a31a249f7e8a9
Henrique Nogueira <henrique.nogueira@ist.utl.pt>
2017-05-07 20:24:38 +0100

    Added update peer loss function, implies global variables inside function definitions

--- clientAPI.c
+++ clientAPI.c
@@ -1,9 +1,48 @@
 /* Header Inclusions                                              */
-#include<stdio.h>
-#include<stdlib.h>
-#include<arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
 #include "clientAPI.h" /* include header with function prototypes */
 #include "messages.h"  /* message description headers */
+
+struct sockaddr_in peer_addr, gw_addr;
+
+/*
+ *  Function:
+ *    update_peer_loss();
+ *
+ *  Description:
+ *    contacts gateway communcating that peer stopped answering
+ *
+ *  Arguments:
+ *      void
+ *
+ *  Return value:
+ *      void
+ *    
+ */
+void update_peer_loss(){
+    int s_gw;
+    message_gw gw_msg;
+
+    strcpy(gw_msg.address, inet_ntoa(peer_addr.sin_addr));
+    gw_msg.type = -1; //comunicate server stopped responding to gateway
+    gw_msg.port = ntohs(peer_addr.sin_port);
+
+    if( (s_gw=socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+        perror("socket error");
+        exit(EXIT_FAILURE);
+    }
+
+    if( (sendto(s_gw, &gw_msg, sizeof(gw_msg), 0,(const struct sockaddr *) &gw_addr, sizeof(gw_addr)) )==-1){
+        perror("GW contact");
+    }
+    close(s_gw);
+    return;
+}
+
+
 
 /*
  *  Function:
@@ -26,7 +65,6 @@
 
 int gallery_connect(char *host, in_port_t port){
     int s_gw, s, scl;
-    struct sockaddr_in gw_addr, srv_addr;
 	socklen_t size_addr;
     message_gw gw_msg;
 
@@ -35,7 +73,7 @@ int gallery_connect(char *host, in_port_t port){
 	gw_addr.sin_port = port;
 	inet_aton(host, &gw_addr.sin_addr);
 	
-	if( (s_gw=socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+	if( (s_gw=socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, 0)) == -1){
 		perror("socket error");
 		exit(EXIT_FAILURE);
 	}
@@ -43,12 +81,17 @@ int gallery_connect(char *host, in_port_t port){
 
 	if( (sendto(s_gw, &gw_msg, sizeof(gw_msg), 0,(struct sockaddr *) &gw_addr, sizeof(gw_addr)) )==-1){
 		perror("GW contact");
-        return -1;
 	}
 	/****** GATEWAY CONTACT ESTABLISHED *****/
 
     size_addr = sizeof(struct sockaddr_in);
-	recvfrom(s_gw, &gw_msg, sizeof(gw_msg), 0, (struct sockaddr *) &gw_addr, &size_addr);
+    sleep(1);
+	if( recvfrom(s_gw, &gw_msg, sizeof(gw_msg), 0, (struct sockaddr *) &gw_addr, &size_addr) == -1){
+        close(s_gw);
+        return -1;
+    }
+    close(s_gw);
+
 	if(gw_msg.type == 0){
 		return 0;
 	}
@@ -58,14 +101,21 @@ int gallery_connect(char *host, in_port_t port){
 		exit(EXIT_FAILURE);
 	}
 
-	srv_addr.sin_family = AF_INET;
-	srv_addr.sin_port = htons(gw_msg.port);
-	inet_aton(gw_msg.address, &srv_addr.sin_addr);
+	peer_addr.sin_family = AF_INET;
+	peer_addr.sin_port = htons(gw_msg.port);
+	inet_aton(gw_msg.address, &peer_addr.sin_addr);
 
-	if( (scl = connect(s, (const struct sockaddr *) &srv_addr, sizeof(struct sockaddr_in))) == -1){
+	if( connect(s, (const struct sockaddr *) &peer_addr, sizeof(struct sockaddr_in)) == -1){
 		perror("connect");
 		exit(EXIT_FAILURE);
 	}
+
+    //get real peer address info
+    size_addr = sizeof(peer_addr);
+    if( getpeername(s, (struct sockaddr *) &peer_addr, &size_addr) == -1){
+        perror("getpeername");
+        exit(EXIT_FAILURE);
+    }
     
     return s;
 }
