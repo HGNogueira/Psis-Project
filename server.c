1356e9d2be44515155d42022834fd95f95996129
Henrique Nogueira <henrique.nogueira@ist.utl.pt>
2017-05-12 12:04:39 +0100

    pfather_interact function with switch condition inside continuous loop, performs basic actions like adding task to the list and adding photo to list

--- server.c
+++ server.c
@@ -5,6 +5,8 @@
 #include <sys/un.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include <string.h>
 #include <signal.h>
 #include <unistd.h>
@@ -14,13 +16,7 @@
 #include "messages.h"
 #define S_PORT 3001
 
-int run = 1, s_gw; //s_gw socket que comunica com gateway (partilhada entre threads)
-struct sockaddr_in gw_addr;
-message_gw gw_msg;
-int ID;            //identificador de servidor atribuido pela gateway
-int updated = 0;   //identifica se peer já foi updated à cadeia de transferêcia de info
-struct pthread_node *thread_list, *thread_head;  //lista de threads
-
+/************************ Functional structures for server.c ******************/
     /* list of threads to easily initiate threads willingly */
 struct pthread_node{
 	pthread_t thread_id;
@@ -36,13 +32,25 @@ typedef struct photo_node{
     int exists; //indicates if photo exists within database or still mereley information about it
     int deleted; //indicates if photo has been deleted
     struct photo_node *next;
-} photo_list;
+} photolist_t;
 
 typedef struct task_node{
-    int ID; //identifies the peer that introduced task into the chain
     task_t task;
     struct task_node *previous;
-} tasklist;
+} tasklist_t;
+/******************************************************************************/
+
+
+/****************** GLOBAL VARIABLES ******************************************/
+int run = 1, s_gw; //s_gw socket que comunica com gateway (partilhada entre threads)
+struct sockaddr_in gw_addr;
+message_gw gw_msg;
+int ID;            //identificador de servidor atribuido pela gateway
+int updated = 0;   //identifica se peer já foi updated à cadeia de transferêcia de info
+struct pthread_node *thread_list, *thread_head;  //lista de threads
+tasklist_t *tasklist;
+photolist_t *photolist;
+/******************************************************************************/
 
 void sigint_handler(int n){
 	run = 0;
@@ -55,8 +63,6 @@ void convert_toupper(char *string){
 		i++;
 	}
 }
-
-
     /* this thread function implements the getting up to date routine */
 void *get_updated(void *thread_scl){
 
@@ -73,11 +79,15 @@ void pson_interact(void *thread_scl){
 }
 
     /* this thread function implements father-peer interaction */
-void *pfather_interact(void *thread_s){
+void *pfather_interact(void *dummy){
     int s, fatherpeer = 2;
+    int err;
     message_gw gw_msg;
     socklen_t addr_len;
     struct sockaddr_in peer_addr;
+    task_t recv_task;
+    tasklist_t *tmp_tasklist;
+    photolist_t *tmp_photolist;
 
     /* wait to know who will be peer father, only thread/occasion where server recfrom gw */
 	recvfrom(s_gw, &gw_msg, sizeof(gw_msg), 0, (struct sockaddr *) &gw_addr, &addr_len);
@@ -97,6 +107,73 @@ void *pfather_interact(void *thread_s){
 	connect(s, (const struct sockaddr *) &peer_addr, sizeof(struct sockaddr_in));
     send(s, &fatherpeer, sizeof(fatherpeer), 0);
 
+    while(1){
+        if( (err = recv(s, &recv_task, sizeof(recv_task), 0)) == -1){
+			perror("recv error");
+            close(s);
+			pthread_exit(NULL);
+		}
+		else if(err == 0){ //peer disconnected
+			printf("Client disconnected from this server\n");
+			gw_msg.type = 3; //indicar à gateway que pai desconectou-se
+			gw_msg.ID = ID;
+			if( (sendto(s_gw, &gw_msg, sizeof(gw_msg), 0,(const struct sockaddr *) &gw_addr, sizeof(gw_addr)) )==-1){
+				perror("GW contact");
+			}
+			close(s);
+            pfather_interact(NULL); //go back to trying to connect with new father
+			return;
+		}
+
+        /* only process task if this peer isn't the one responsible for it */
+        if(recv_task.ID != ID ){
+            /* process new task */
+            tmp_tasklist = (tasklist_t*) malloc(sizeof(tasklist_t));
+            switch(recv_task.type){
+                case -1:
+                    printf("Deleting photo with id=%"PRIu64"\n", recv_task.photo_id);
+                    tmp_tasklist->task = recv_task;
+
+                    /* delete photo */
+
+                    break;
+                case 0:
+                    printf("Adding keyword to photo with id=%"PRIu64"\n", recv_task.photo_id);
+                    tmp_tasklist->task = recv_task;
+
+                    /* add keyword to keyword list */
+
+                    break;
+                case 1:
+                    printf("Adding new photo with id=%"PRIu64"\n", recv_task.photo_id);
+                    tmp_tasklist->task = recv_task;
+                    /* add photo to photolist */
+                    tmp_photolist = (photolist_t*) malloc(sizeof(photolist_t));
+                    tmp_photolist->photo_id = recv_task.photo_id;
+                    strcmp(tmp_photolist->photo_name, recv_task.photo_name);
+                    tmp_photolist->photo_size = recv_task.photo_size;
+                    tmp_photolist->exists = 0;
+                    tmp_photolist->deleted = 0;
+
+                    /*######### NEED GUARD #######*/
+                    tmp_photolist->next = photolist;
+                    photolist = tmp_photolist;
+
+
+                    break;
+                default:
+                    printf("Unknown routine for task with type %d\n", recv_task.type);
+                    free(tmp_tasklist);
+                    continue;
+            }
+        }
+
+        /* insert tmp_list to head of task list */ /*###### NEED GUARD #### */
+        tmp_tasklist->previous = tasklist;
+        tasklist = tmp_tasklist; 
+
+        /* signal pson thread to propagate this task */
+    }
 }
 
     /* this thread function implements to client-peer interaction */
@@ -234,8 +311,13 @@ int main(){
 	recvfrom(s_gw, &ID, sizeof(ID), 0, (struct sockaddr *) &gw_addr, &addr_len);
 	printf("I was assign ID=%d\n", ID);
 
+    //initialize tasklist
+    tasklist = NULL;
+    //initialize photolise
+    photolist = NULL;
+
     //initiate new father peer interaction thread
-    if( pthread_create(&(thread_list->thread_id) , NULL, pfather_interact, &(thread_list->s)) != 0)
+    if( pthread_create(&(thread_list->thread_id) , NULL, pfather_interact, NULL) != 0)
 				printf("Error creating a new thread\n");
 
     thread_list->next = (struct pthread_node *) malloc(sizeof(struct pthread_node));
