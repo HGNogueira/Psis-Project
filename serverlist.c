604022dac589bff4c7fd0dce72747efb2820d8f0
Henrique Nogueira <henrique.nogueira@ist.utl.pt>
2017-05-14 15:37:45 +0100

    serverlist with doubly linked looped list implemented

--- serverlist.c
+++ serverlist.c
@@ -23,13 +23,15 @@ void add_server(serverlist **peers, char *address, int port, int ID, pthread_rwl
 
     pthread_rwlock_wrlock(rwlock); //must lock whole region, DANGER
     if(*peers == NULL){
+        newnode->prev = newnode; //previous node is itself
         newnode->next = newnode; //next node is itself
         *peers = newnode;
     }
     else{
         auxnode = (*peers)->next;
-        (*peers)->next = newnode;
         newnode->next = auxnode;
+        newnode->prev = (*peers);
+        (*peers)->next = newnode;
     }//*server keeps pointing to the same node
     pthread_rwlock_unlock(rwlock);
     
@@ -66,52 +68,60 @@ serverlist *pick_server(serverlist **peers, pthread_rwlock_t *rwlock){
 /* searches serverlist and deletes node, if not found 0 is returned, if serverlist empty -1
  * is returned, if success 1 is returned */
 int delete_peer(serverlist **peers, char *address, int port, pthread_rwlock_t *rwlock){
-	struct node *s1, *s2, *auxnode;
+	struct node *s1, *auxnode;
+    int startID;
     
     pthread_rwlock_wrlock(rwlock);
 	s1 = *peers;
 	if(s1 == NULL)
 		return -1;
-	if( strcmp(s1->address, address) == 0){
-            if( s1->port == port){
-                *peers = s1->next;
-                free(s1);
-                pthread_rwlock_unlock(rwlock);
-                return 1;
-            }
+    if(s1->next == s1){/* list with only one element */
+        free(s1);
+        *peers = NULL;
+        return 1;
     }
 
-	while(s1->next != NULL){
-		if( strcmp(s1->next->address, address) == 0){
+    startID = s1->ID;
+    s1 = s1->next;
+
+	while(1){
+		if( strcmp(s1->address, address) == 0){
             if( s1->port == port){
-                s2 = s1->next;
-                s1->next = s2->next;
-                free(s2);
+                (s1->prev)->next = s1->next;
+                (s1->next)->prev = s1->prev;
+                free(s1);
                 pthread_rwlock_unlock(rwlock);
                 return 1;
             }
 
         }
+        if(s1->next->ID == startID)
+            break;
 		s1 = s1->next;
 	}
     pthread_rwlock_unlock(rwlock);
 	return 0;
 }
 
-serverlist *search_server(serverlist **servers, int ID, pthread_rwlock_t *rwlock){
+serverlist *search_server(serverlist **peers, int ID, pthread_rwlock_t *rwlock){
 	struct node *s;
+    int startID;
     
     pthread_rwlock_rdlock(rwlock);
-	s = *servers;
+	s = *peers;
 	if(s == NULL){
         pthread_rwlock_unlock(rwlock);
 		return NULL;
     }
-	while(s != NULL){
+
+    startID = s->ID;
+	while(1){
 		if(s->ID == ID){
             pthread_rwlock_unlock(rwlock);
 			return s;
         }
+        if(s->next->ID ==startID)
+            break;
 		s = s->next;
 	}
     pthread_rwlock_unlock(rwlock);
@@ -119,20 +129,25 @@ serverlist *search_server(serverlist **servers, int ID, pthread_rwlock_t *rwlock
 }
 
 /* to be implemented ####.... */
-serverlist *search_father(serverlist **servers, int ID, pthread_rwlock_t *rwlock){
+serverlist *search_father(serverlist **peers, int ID, pthread_rwlock_t *rwlock){
 	struct node *s;
+    int startID;
     
     pthread_rwlock_rdlock(rwlock);
-	s = *servers;
+	s = *peers;
 	if(s == NULL){
         pthread_rwlock_unlock(rwlock);
 		return NULL;
     }
-	while(s != NULL){
+
+    startID = s->ID;
+	while(1){
 		if(s->ID == ID){
             pthread_rwlock_unlock(rwlock);
-			return s;
+			return s->prev;
         }
+        if(s->next->ID ==startID)
+            break;
 		s = s->next;
 	}
     pthread_rwlock_unlock(rwlock);
