231da584ab1cc43d173e34140d4d200433ea0cc5
HGNogueira <henrique.nogueira@tecnico.ulisboa.pt>
2017-05-17 09:30:50 +0100

    Initial function prototypes for photolist

---
+++ photolist.h
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <sys/types.h>
+
+    /* prototype for photo list node, contains all information regarding each 
+     * photo as well as extras for replication purposes */
+typedef struct photo_node_t{
+    uint32_t photo_id;
+    char photo_name[50];
+    unsigned photo_size;
+    int deleted;
+    struct photo_node_t *next;
+} photolist_t;
+
+
+/* initialize photolist_t */
+photolist_t *photolist_init();
+/* add photo to photolist, to be inserted in sorted order according to photo_id */
+photolist_t *photolist_insert(photolist_t **photos, pthread_rwlock_t *rwlock);
+
+void photolist_add_keyword(photolist_t **photos, char *keyword, pthread_rwlock_t *rwlock);
+int photolist_delete(photolist_t **photos, pthread_rwlock_t *rwlock);
+int photolist_search(photolist_t **photos, uint32_t photo_id, pthread_rwlock_t *rwlock);
+
