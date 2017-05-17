7bfea15ed0a159ebb30be87f28b57b68a425c64a
HGNogueira <henrique.nogueira@tecnico.ulisboa.pt>
2017-05-17 11:03:40 +0100

    Changed photolist_insert return value to int

--- photolist.h
+++ photolist.h
@@ -1,5 +1,8 @@
 #include <pthread.h>
 #include <sys/types.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <pthread.h>
 
     /* prototype for photo list node, contains all information regarding each 
      * photo as well as extras for replication purposes */
@@ -8,6 +11,7 @@ typedef struct photo_node_t{
     char photo_name[50];
     unsigned photo_size;
     int deleted;
+    struct photo_node_t *prev;
     struct photo_node_t *next;
 } photolist_t;
 
@@ -15,7 +19,7 @@ typedef struct photo_node_t{
 /* initialize photolist_t */
 photolist_t *photolist_init();
 /* add photo to photolist, to be inserted in sorted order according to photo_id */
-photolist_t *photolist_insert(photolist_t **photos, pthread_rwlock_t *rwlock);
+int photolist_insert(photolist_t **photos, uint32_t photo_id, char *photo_name, unsigned photo_size, pthread_rwlock_t *rwlock);
 
 void photolist_add_keyword(photolist_t **photos, char *keyword, pthread_rwlock_t *rwlock);
 int photolist_delete(photolist_t **photos, pthread_rwlock_t *rwlock);
